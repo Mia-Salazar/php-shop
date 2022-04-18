f85962101e1cc85a24f12b65984e1c0a3c43623f
Mia-Salazar <maria.sgr@gmail.com>
2022-04-18 19:09:22 +0200

    Login

--- index.php
+++ index.php
@@ -6,13 +6,24 @@
 	<title>Index.php</title>
 </head>
 <body>
-
+	<form method="post" action="index.php" class="form">
+		<label class="label" for="FullName">Nombre</label>
+		<input class="input" id="FullName" placeholder="escribe tu nombre" name="FullName" type="text" />
+		<label class="label" for="Email">Email</label>
+		<input class="input" id="Email" placeholder="escribe tu email" name="Email" type="email" />
+		<button class="button" type="submit">Enviar</button>
+	</form>	
 	<?php
-	
 		include "consultas.php";
+		include "utilidades.php";
+		if(isset($_POST["FullName"]) && isset($_POST["Email"])) {
+			$userType = tipoUsuario($_POST["FullName"], $_POST["Email"]);
+			if ($userType !== "no registrado") {
+				setcookie("userType", $userType, time() + 7200);
+			}
+			getLoginStructure($userType, $_POST["FullName"]);
+		}
 
 	?>
-	
-	
 </body>
 </html>
---
+++ utilidades.php
@@ -0,0 +1,24 @@
+<?php
+	function getLoginStructure($userType, $name) {
+		if ($userType === "superadmin") {
+			echo "
+			<h1> Te damos la bienvenida, " . $name . "</h1>
+			<a href='usuarios.php'>Ir a la lista de usuarios</a>
+			";
+		} else if ($userType === "autorizado") {
+			echo "
+			<h1> Te damos la bienvenida, " . $name . "</h1>
+			<a href='articulos.php'>Ir a la lista de artículos</a>
+			";
+		} else if ($userType === "registrado") {
+			echo "
+			<h1> Te damos la bienvenida, " . $name . "</h1>
+			<h2>No tienes permisos para acceder</h2>
+			";
+		} else {
+			echo "
+			<h1> Te damos la bienvenida Usuario no registrado</h1>
+			";
+		}
+	}
+?>
