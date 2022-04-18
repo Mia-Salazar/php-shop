ac0d13628859bbf2d3b284875a3b494f76780cc9
Mia-Salazar <maria.sgr@gmail.com>
2022-04-18 20:53:00 +0200

    Add user page

--- usuarios.php
+++ usuarios.php
@@ -9,10 +9,26 @@
 <body>
 
 	<?php 
-
+		include "utilidades.php";
 		include "funciones.php";
+		if (isset($_COOKIE['userType']) && $_COOKIE['userType'] === "superadmin") {
+			echo "<h1>Página de usuarios</h1>";
+			echo "<h2>Permisos actuales: " . getPermisos() . "</h2>";
+			echo "
+				<form action='usuarios.php' method='post'>
+					<input name='permissionType' value='" .  getPermisos() . "' class='button'>
+					<button type='submit'class='button'>Cambiar valor de permisos</button>
+				</form>
+			";
+			if (isset($_POST["permissionType"])) {
+				cambiarPermisos();
+			}
+			getUsersList();
+
+		}
 
 	?>
+	<a href="index.php" class="navigation">Volver atrás</a>
 
 </body>
 </html>
