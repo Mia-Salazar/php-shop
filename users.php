14c4363498664f6711aedb691d908bb9eeb47c76
Mia-Salazar <maria.sgr@gmail.com>
2022-05-29 22:04:11 +0200

    Add styles to login

--- users.php
+++ users.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<link rel="stylesheet" type="text/css" href="general.css">
 	<title>Usuarios</title>
 </head>
 <body>
